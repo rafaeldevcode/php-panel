bde4672652a48db1413296f2e2e4bb75315fb995
Rafael Vieira <rafaeldevcode@gmail.com>
2023-07-08 09:42:40 -0300

    <refactor>[required]: adding '*' to required input label

--- partials/form/input-checkbox-switch.php
+++ partials/form/input-checkbox-switch.php
@@ -1,12 +1,16 @@
 <?php 
+    $required = '';
+
     if(isset($attributes)):
         if(is_array($attributes)):
             $attr = '';
             foreach($attributes as $indice => $attribute):
                 $attr .= "{$indice}={$attribute} ";
+                $required = $indice == 'required' ? '*' : '';
             endforeach;
         else:
             $attr = $attributes;
+            $required = $attributes == 'required' ? '*' : '';
         endif;
     else:
         $attr = '';
@@ -20,6 +24,9 @@
 
 <div class='form-check form-switch'>
     <input class='form-check-input' type='checkbox' id='<?php echo $name ?>' name='<?php echo $name ?>' <?php echo $checked ?> <?php echo $attr ?>>
-    <label class='form-check-label' for='<?php echo $name ?>'><?php echo $label ?></label>
+    <label class='form-check-label' for='<?php echo $name ?>'>
+        <?php echo $label ?>
+        <span class="text-cm-danger"><?php echo $required ?></span>
+    </label>
     <span class='position-absolute end-0 bottom-0 validit'></span>
 </div>
